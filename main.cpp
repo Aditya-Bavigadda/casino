9b379d28991e6daa39b9219879937c0d3cc37fd7
Aditya-Bavigadda <adityabavigadda@gmail.com>
2021-08-29 17:39:28 +1000

    finished the login and registration completely, now game is left
    Signed-off-by: Aditya-Bavigadda <adityabavigadda@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,6 +4,30 @@
 #include<fstream>
 #include<limits>
 
+void check_balance(std::string user_name, std::string password) //checks the balance of the user, and if the user has a balance less than 100, they automatically get 1000 coins to play with
+{
+    std::string foo; //garbage variable for the username and password
+    int balance; //stores balance
+    std::ifstream user_details(user_name); //opens the user name file
+    user_details >> foo; //username
+    user_details >> foo; //password
+    user_details >> balance; //balance
+    if(balance < 100) 
+    {
+        std::cout << "you broke bro, here I'll reset your balance to 1000"; 
+        std::ofstream user_details1(user_name); //creates a new file for the user
+        user_details1 << user_name << "\n" << password << "\n1000"; //rewrites everything
+    }
+    else
+    {
+        std::cout << "you can play the game bro";
+    }
+}
+
+void play_game(std::string user_name) //plays the game
+{
+    
+}
 void registration_process(std::vector<std::string> unnacceptable_words)
 {
     bool exit_loop = false;
@@ -34,7 +58,7 @@ void registration_process(std::vector<std::string> unnacceptable_words)
     } while (exit_loop == false);
     std::cout << "congrats, you have been registered\n";
     std::ofstream file(username); //creates new file for every user that is registered
-    file << username << "\n" << password; //add username and password to file
+    file << username << "\n" << password << "\n0"; //add username and password to file and gives them the starting balance of 0
 }
 
 void login_process()
@@ -49,7 +73,15 @@ void login_process()
     std::string user, pass; //used to get data from the file
     std::getline(file, user); //gets username on first line
     std::getline(file, pass); //gets password on next line
-    user == username && pass == password ? std::cout << "you have logged in, welcome " << username << "\n" : std::cout << "invalid username or password\n"; //checks validity of user credentials
+    if(user == username && pass == password)
+    {
+        std::cout << "you have logged in, welcome " << username << "\n";
+        check_balance(username, password); //checks the balance of the username (felt it would be better to do it here instead of giving the password variable to the play_game function)
+    }
+    else
+    {
+        std::cout << "invalid username or password\n";
+    }    
 }
 
 int main()
